df2a212ebd916ac41fd4e93ed5eae111ad2b13fe
Naymur000 <nayeemur000@gmail.com>
2026-02-21 23:38:33 +0600

    t

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+
 
 const Login = () => {
+
+    const submitHandler = (e)=>{
+        e.preventDefault()
+        console.log("Form submitted")
+    }
+
     return (
         <div className='flex items-center justify-center h-screen w-screen'>
-            <div className='border-2 border-red-600'>
-                <form className='flex flex-col items-center justify-center'>
-                    <input type="email" placeholder='enter your email' />
-                    <input type="password" placeholder='enter your password' />
-                    <button>Log in</button>
+            <div className='border-2 border-emerald-900 p-12 rounded-xl '>
+                <form onSubmit={(e)=>{
+                    submitHandler(e)
+                }} className='flex flex-col items-center justify-center'>
+                    <input required className='text-2xl outline-none border-2 border-emerald-600 py-1 px-10 rounded-full my-2 bg-transparent placeholder:text-gray-400' type="email" placeholder='enter your email' />
+                    <input required className='text-2xl outline-none border-2 border-emerald-600 py-1 px-10 rounded-full my-2 bg-transparent placeholder:text-gray-400' type="password" placeholder='enter your password' />
+                    <button className='text-2xl outline-none border-none bg-emerald-600 py-1 px-10 rounded-full my-2'>Log in</button>
                 </form>
             </div>
             
