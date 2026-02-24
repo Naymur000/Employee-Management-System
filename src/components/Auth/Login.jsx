99c778d5fcfa1e305487e8494734c3a70b98e627
Naymur000 <nayeemur000@gmail.com>
2026-02-24 21:56:28 +0600

    c

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,10 +1,13 @@
 // import { useState } from "react";
 
+import { useState } from "react";
+
 
 const Login = () => {
 
    
-    
+    const [email , setEmail] = useState('')
+    const [password, setPassword] = useState('');
 
     const submitHandler = (e)=>{
         e.preventDefault()
@@ -18,12 +21,16 @@ const Login = () => {
                     submitHandler(e)
                 }} 
                 className='flex flex-col items-center justify-center'>
-                    <input required className='text-2xl outline-none border-2 border-emerald-600 py-1 px-10 rounded-full my-2 bg-transparent placeholder:text-gray-400' type="email" placeholder='enter your email' />
+                    <input 
+                    onChange={(e)=> {
+                        console.log(e)
+                    }}
+                    required 
+                    className='text-2xl outline-none border-2 border-emerald-600 py-1 px-10 rounded-full my-2 bg-transparent placeholder:text-gray-400' type="email" placeholder='enter your email' />
                     <input required className='text-2xl outline-none border-2 border-emerald-600 py-1 px-10 rounded-full my-2 bg-transparent placeholder:text-gray-400' type="password" placeholder='enter your password' />
                     <button className='text-2xl outline-none border-none bg-emerald-600 py-1 px-10 rounded-full my-2'>Log in</button>
                 </form>
             </div>
-            <p>Hello</p>
              
         </div>
     );
